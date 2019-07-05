f45e819b61411928a58a0a99646e81f21f91ba1e
Mike Innes <mike.j.innes@gmail.com>
2019-07-05 17:07:25 +0100

    copy task

--- src/Continuations.jl
+++ src/Continuations.jl
@@ -8,7 +8,8 @@ function reset(f)
   c = current_task()
   t = Task() do
     task_local_storage(:reset, c)
-    yieldto(c, f())
+    y = f()
+    yieldto(task_local_storage(:reset), y)
   end
   yieldto(t)
 end
@@ -19,7 +20,13 @@ end
 
 function shift(f)
   c = current_task()
-  yieldto(task_local_storage(:reset), f(x -> yieldto(c, x)))
+  r = task_local_storage(:reset)
+  t = Task() do
+    yieldto(r, f(x -> yieldto(copy(c), (x, current_task()))))
+  end
+  y, ret = yieldto(t)
+  task_local_storage(:reset, ret)
+  return y
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,23 @@ using Continuations, Test
 k = @reset shift(k -> k)+1
 
 @test k(1) == 2
+@test k(1) == 2
+
+@test @reset(2*shift(k -> k(4))) == 8
+@test @reset(2*shift(k -> k(k(4)))) == 16
+
+@test @reset(begin
+  for i = 1:5
+    _ = shift(k -> (i,k(nothing)))
+  end
+  ()
+end) == (1, (2, (3, (4, (5, ())))))
+
+quantum_predicate(x) = shift(k -> (k(true), k(false)))
+
+function foo(x)
+  quantum_predicate(x) && (x = x .+ 2)
+  2 .* x
+end
+
+@test @reset(foo([1,2,3])) == ([6, 8, 10], [2, 4, 6])
